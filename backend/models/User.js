ab43663810fbde0b841fbbc85dd9c1d995890d0f
ShubhamRGavhane <shubhamgavhane05@gmail.com>
2026-02-09 19:23:05 +0530

    formatted the code

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
