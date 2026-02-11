48cd0780c582925add0be5110f31b749b3ac7040
ShubhamRGavhane <shubhamgavhane05@gmail.com>
2026-02-11 16:26:48 +0530

    solved issue

--- backend/app.js
+++ backend/app.js
@@ -11,10 +11,13 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI);
+console.log("MONGO_URI:", process.env.MONGO_URI);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
   console.log("Server running on port 5000");
 });
